4168ecd6182389fd7a21a0677b2a3965e7a2ff9c
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-01-08 10:52:10 +0000

    ajustes nos controladores e rotas

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,5 +8,29 @@ use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+  use AuthorizesRequests, ValidatesRequests;
+
+  private Array $data;
+
+  public function __construct()
+  {
+    $this->data = array();
+  }
+
+  protected function title(String $title = Null): void
+  {
+    $this->data['title'] = ($title == Null? 'Achieve Rent A Car' : $title.' - Achieve Rent A Car');
+  }
+
+  protected function current(String $current): void
+  {
+    $this->data['current'] = $current;
+  }
+
+  protected function load($view)
+  {
+    if(!array_key_exists('title', $this->data)) $this->title();
+    return view($view, $this->data);
+  }
+
 }
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+@extends('layout')
+@section('main')
+  <h1>Home</h1>
+@endsection
