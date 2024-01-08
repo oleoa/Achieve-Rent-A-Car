18dbf6d5e2b7732c13f7438620e23023e6f62482
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-01-08 17:58:08 +0000

    API showing how many views

--- app/Http/Controllers/API.php
+++ app/Http/Controllers/API.php
@@ -5,10 +5,14 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Views;
+
 class API extends Controller
 {
   public function index()
   {
+    $views = Views::all()->toArray();
+    dd($views);
     return [1,2,3];
   }
 }
