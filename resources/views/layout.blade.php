056afd665c329bf7c8a460262d6f7ac79604525b
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-01-09 19:43:16 +0000

    sidebar content updated

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -59,17 +59,33 @@
         <a href="{{route('home', $locale)}}"><img src="{{url('/img/logo2.png')}}" alt="Logo Achieve Rent A Car" class="h-10 pl-6"></a>
 
         <!-- Sidebar Itself -->
-        <nav id="sidebar" class="bg-zinc-800 text-white h-screen w-48 fixed top-0 left-0 transition-transform duration-300 transform -translate-x-full">
-          <!-- Sidebar content goes here -->
-          <div class="p-4">
-              <span class="text-lg font-semibold">Sidebar</span>
-              <button id="closeSidebar" class="text-white focus:outline-none">
-                  <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
-                  </svg>
-              </button>
+        <nav id="sidebar" class="bg-zinc-800 text-white h-screen w-44 fixed top-0 left-0 transition-transform duration-300 transform -translate-x-full">
+          
+          <!-- Close Button -->
+          <div class="p-4 flex items-center justify-end fixed top-0 left-0 w-44">
+            <button id="closeSidebar" class="text-white">
+              <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+              </svg>
+            </button>
           </div>
-          <!-- Add additional sidebar links or content here -->
+                    
+          <!-- Items -->
+          <a class="@if($current == 'home') text-blue-400 underline @endif" href="{{route('home', $locale)}}"><h4 class="p-4">@lang('Home')</h4></a>
+          <a class="@if($current == 'about') text-blue-400 underline @endif" href="{{route('about', $locale)}}"><h4 class="p-4">@lang('About')</h4></a>
+          <a class="@if($current == 'faq') text-blue-400 underline @endif" href="{{route('faq', $locale)}}"><h4 class="p-4">@lang('FAQ')</h4></a>
+          <a class="@if($current == 'contact') text-blue-400 underline @endif" href="{{route('contact', $locale)}}"><h4 class="p-4">@lang('Contact')</h4></a>
+
+          <!-- Flags -->
+          <div class="flex justify-start items-center gap-4 p-4">
+            <a href="{{route($current, ['en'])}}">
+              <img src="{{ asset('img/en.png') }}" alt="English Flag" class="w-6 h-4">
+            </a>
+            <a href="{{route($current, ['pt'])}}">
+              <img src="{{ asset('img/pt.png') }}" alt="Portuguese Flag" class="w-6 h-4">
+            </a>
+          </div>
+
         </nav>
 
       </div>
