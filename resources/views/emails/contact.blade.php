f4c39c5783e9dc1927460ffd4915a4a8f3a0285e
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-01-10 11:08:57 +0000

    icons and email format

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>New Contact Form Submission</title>
+      <title>Novo Contato Do Site Achieve</title>
     </head>
     <body>
-        <h1>New Contact Form Submission</h1>
-        <p><strong>Name:</strong> {{ $data['name'] }}</p>
-        <p><strong>Email:</strong> {{ $data['email'] }}</p>
-        <p><strong>Message:</strong> {{ $data['message'] }}</p>
+      <h1>Novo Contato Do Site Achieve</h1>
+      <p><strong>Name:</strong> {{ $data['name'] }}</p>
+      <p><strong>Email:</strong> {{ $data['email'] }}</p>
+      <p><strong>Prefix:</strong> {{ $data['prefix'] }}</p>
+      <p><strong>Phone:</strong> {{ $data['phone'] }}</p>
+      <p><strong>Message:</strong> {{ $data['message'] }}</p>
     </body>
 </html>
