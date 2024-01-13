ba63d512395bc2b9c3a1deca156a6e84cf9a2a98
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-01-13 11:01:47 +0000

    home page title max size

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,13 @@
 @extends('layout')
 @section('main')
-  <main class="">
+
+  <main class="h-screen">
 
     <!-- Image -->
-    <div style="background-image: url('/img/home.jpeg')" class="h-96 flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center">
-      <h1 class="text-white text-center font-black text-6xl">@lang('Home Page')</h1>
+    <div style="background-image: url('/img/home.jpeg')" class="h-2/3 flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center">
+      <h1 class="text-white text-center font-black text-6xl">@lang('Achieve Rent A Car')</h1>
     </div>
 
   </main>
+
 @endsection
