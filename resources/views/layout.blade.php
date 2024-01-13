6d91049a5799dfaa9ac4430d7adbfd993ec3ae51
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-01-13 13:20:12 +0000

    loader loading on sidebar and home title margin for phone and not loading loader

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -73,23 +73,23 @@
 
             <!-- Items -->
             <div class="flex justify-between pr-4">
-              <a class="@if($current == 'home') text-blue-400 underline @endif" href="{{route('home', $locale)}}"><h4 class="p-4">@lang('Home')</h4></a>
+              <a class="loaderActivator @if($current == 'home') text-blue-400 underline @endif" href="{{route('home', $locale)}}"><h4 class="p-4">@lang('Home')</h4></a>
               <button id="closeSidebar" class="text-white">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
                 </svg>
               </button>
             </div>
-            <a class="@if($current == 'about') text-blue-400 underline @endif" href="{{route('about', $locale)}}"><h4 class="p-4">@lang('About')</h4></a>
-            <a class="@if($current == 'faq') text-blue-400 underline @endif" href="{{route('faq', $locale)}}"><h4 class="p-4">@lang('FAQ')</h4></a>
-            <a class="@if($current == 'contact') text-blue-400 underline @endif" href="{{route('contact', $locale)}}"><h4 class="p-4">@lang('Contact')</h4></a>
+            <a class="loaderActivator @if($current == 'about') text-blue-400 underline @endif" href="{{route('about', $locale)}}"><h4 class="p-4">@lang('About')</h4></a>
+            <a class="loaderActivator @if($current == 'faq') text-blue-400 underline @endif" href="{{route('faq', $locale)}}"><h4 class="p-4">@lang('FAQ')</h4></a>
+            <a class="loaderActivator @if($current == 'contact') text-blue-400 underline @endif" href="{{route('contact', $locale)}}"><h4 class="p-4">@lang('Contact')</h4></a>
 
             <!-- Flags -->
             <div class="flex justify-start items-center gap-4 p-4">
-              <a href="{{route($current, ['en'])}}">
+              <a href="{{route($current, ['en'])}}" class="loaderActivator">
                 <img src="{{ asset('img/en.png') }}" alt="English Flag" class="w-6 h-4">
               </a>
-              <a href="{{route($current, ['pt'])}}">
+              <a href="{{route($current, ['pt'])}}" class="loaderActivator">
                 <img src="{{ asset('img/pt.png') }}" alt="Portuguese Flag" class="w-6 h-4">
               </a>
             </div>
