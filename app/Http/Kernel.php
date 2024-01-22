2a436e0fd23c6f91681ecbc6a34ccce81c307efd
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-01-22 10:24:15 +0000

    middlewares

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -35,6 +35,8 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
+            \App\Http\Middleware\MyAuthenticate::class,
+            \App\Http\Middleware\IsAdmin::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
---
+++ app/Http/Middleware/MyAuthenticate.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Support\Facades\Auth;
+
+class MyAuthenticate
+{
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+   */
+  public function handle(Request $request, Closure $next): Response
+  {
+    $currentRouteName = $request->route()->getName();
+    $allowedNotAuthRoutes = [
+      "sign.in",
+      "sign.out",
+      "sign.ing-in",
+      "user.setup",
+      "user.settingup"
+    ];
+
+    if(in_array($currentRouteName, $allowedNotAuthRoutes))      
+      return $next($request);
+
+    if(Auth::guest())
+      return redirect()->route('sign.in');
+
+    return $next($request);
+  }
+}
