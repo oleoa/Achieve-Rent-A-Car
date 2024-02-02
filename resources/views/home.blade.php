1c22a67ea13b34382d64e0704c89dd6c1d8b3cc7
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-02-02 10:31:45 +0000

    cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,21 +17,18 @@
     </div>
 
     <!-- Cards -->
-    <div class="grid xl:grid-cols-2 grid-cols-1 w-full xl:px-margin px-4 py-4 gap-4">
+    <div class="grid xl:grid-cols-3 grid-cols-1 w-full xl:px-margin px-4 py-4 gap-4">
 
-        <h2 class="xl:col-span-2 text-center font-bold text-4xl">@lang('Home-Section.1-Title')</h2>
+        <h2 class="xl:col-span-3 text-center font-bold text-4xl">@lang('Home-Section.1-Title')</h2>
 
-        @for($i = 1; $i <= 2; $i++)
+        @for($i = 1; $i <= 3; $i++)
             <div class="rounded-lg shadow-lg">
-                <img src="https://media.wired.com/photos/5fb70f2ce7b75db783b7012c/master/pass/Gear-Photos-597589287.jpg" alt="" class="rounded-t-lg">
+                <img src="{{asset('/img/cards/'.$i.'.png')}}" alt="" class="rounded-t-lg">
                 <div class="p-4">
-                <h3 class="text-2xl font-bold text-gray-800">@lang('Home-Section.1-Card.'.$i.'-Title')</h3>
-                <p class="mt-2 text-gray-600">
-                    @lang('Home-Section.1-Card.'.$i.'-Paragraph')
-                </p>
-                </div>
-                <div class="flex justify-end items-center w-full p-4">
-                <a href="" class="bg-gray-800 text-white px-4 py-2 rounded hover:bg-gray-700 hover:text-white">@lang('Button')</a>
+                    <h3 class="text-2xl font-bold text-gray-800">@lang('Home-Section.1-Card.'.$i.'-Title')</h3>
+                    <p class="mt-2 text-gray-600">
+                        @lang('Home-Section.1-Card.'.$i.'-Paragraph')
+                    </p>
                 </div>
             </div>
         @endfor
@@ -39,7 +36,7 @@
     </div>
 
     <!-- Separator -->
-    <div class="py-4 flex items-center justify-center xl:px-margin px-4">
+    <div class="py-8 flex items-center justify-center xl:px-margin px-4">
       <div class="xl:w-1/3 w-full">
         <x-separator/>
       </div>
