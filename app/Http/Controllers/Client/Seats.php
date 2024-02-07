a3ad48a77deecd04b8ea43b82ce4022cd43f89f9
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-02-07 11:00:40 +0000

    seats page beta

--- app/Http/Controllers/Client/Seats.php
+++ app/Http/Controllers/Client/Seats.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Client;
 
-use App\Http\Controllers\Dashboard\Controller;
+use App\Http\Controllers\Client\Controller;
 use Illuminate\Http\Request;
 
 class Seats extends Controller
