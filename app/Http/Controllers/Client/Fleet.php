303d39c2c609485819ef3d8a17e53d4402371525
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-02-14 11:58:15 +0000

    fleet in 404

--- app/Http/Controllers/Client/Fleet.php
+++ app/Http/Controllers/Client/Fleet.php
@@ -9,7 +9,8 @@ use App\Helpers\AchieveAPI;
 class Fleet extends Controller
 {
     public function index($locale)
-    {        
+    {
+        return redirect()->route('home', ['locale' => $locale]);
         $api = new AchieveAPI();
         $fleet = $api->fleet();
         $this->data('fleet', $fleet['fleets'][0]['groups']);
