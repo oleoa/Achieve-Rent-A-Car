a182371225e842b72fb469378331356739a22f26
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-02-19 14:02:04 +0000

    let's try it on the server

--- resources/js/fleet.js
+++ resources/js/fleet.js
@@ -1,3 +1,4 @@
+var iframe = document.getElementById('anyrentBookingsIframe');
 updateIframeHeight();
 
 window.addEventListener('resize', function () {
@@ -5,9 +6,22 @@ window.addEventListener('resize', function () {
 });
 
 function updateIframeHeight() {
-    var iframe = document.getElementById('anyrentBookingsIframe');
     var fleetLength = document.getElementById('fleetLength').value;
     const screenWidth = window.innerWidth;
     var rows = screenWidth <= 1008 ? fleetLength : Math.ceil(fleetLength/3) ;
     iframe.style.height = 430*rows+'px';
+}
+
+iframe.onload = function() {
+
+    var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
+
+    var btns = innerDoc.querySelectorAll('button');
+    
+    for (var i = 0; i < btns.length; i++) {
+        btns[i].addEventListener('click', function() {
+            window.scrollBy(100, 0);
+        });
+    }
+
 }
--- resources/views/client/fleet.blade.php
+++ resources/views/client/fleet.blade.php
@@ -7,7 +7,7 @@
     </div>
 
     <!-- Fleet -->
-    <div class="w-full py-4 xl:p-4">
+    <div class="w-full">
         <input type="hidden" value="{{count($fleet)}}" id="fleetLength">
         <iframe src="https://reservas.achieverentacar.com/pt/frota?iframe" scrolling="no" id="anyrentBookingsIframe" style="width: 1px; min-width: 100%; border: 0px; overflow: hidden;"></iframe>
     </div>
