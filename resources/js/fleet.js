98bb0db6e66526174666f0d5d735b2c0eec256d7
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-02-19 13:41:38 +0000

    Math.ceil to the rows number

--- resources/js/fleet.js
+++ resources/js/fleet.js
@@ -8,6 +8,6 @@ function updateIframeHeight() {
     var iframe = document.getElementById('anyrentBookingsIframe');
     var fleetLength = document.getElementById('fleetLength').value;
     const screenWidth = window.innerWidth;
-    var rows = screenWidth <= 1008 ? fleetLength : fleetLength/3 ;
+    var rows = screenWidth <= 1008 ? fleetLength : Math.ceil(fleetLength/3) ;
     iframe.style.height = 430*rows+'px';
 }
