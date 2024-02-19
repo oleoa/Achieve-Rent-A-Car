074edc22c3f5efc26e91e509e7b33d09ed9ec55b
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-02-19 14:05:15 +0000

    no need to scroll up

--- resources/js/fleet.js
+++ resources/js/fleet.js
@@ -1,4 +1,5 @@
 var iframe = document.getElementById('anyrentBookingsIframe');
+var fleetLength = document.getElementById('fleetLength').value;
 updateIframeHeight();
 
 window.addEventListener('resize', function () {
@@ -6,22 +7,7 @@ window.addEventListener('resize', function () {
 });
 
 function updateIframeHeight() {
-    var fleetLength = document.getElementById('fleetLength').value;
     const screenWidth = window.innerWidth;
     var rows = screenWidth <= 1008 ? fleetLength : Math.ceil(fleetLength/3) ;
     iframe.style.height = 430*rows+'px';
-}
-
-iframe.onload = function() {
-
-    var innerDoc = iframe.contentDocument || iframe.contentWindow.document;
-
-    var btns = innerDoc.querySelectorAll('button');
-    
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener('click', function() {
-            window.scrollBy(100, 0);
-        });
-    }
-
 }
