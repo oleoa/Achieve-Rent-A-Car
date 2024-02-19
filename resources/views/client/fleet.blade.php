3558946383705c1f3172b615768e2c16fbee9523
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-02-19 14:19:15 +0000

    new image and translations

--- resources/views/client/fleet.blade.php
+++ resources/views/client/fleet.blade.php
@@ -2,14 +2,14 @@
 @section('main')
 
     <!-- Banner -->
-    <div style="background-image: url('/img/fleet/banner.png')" class="h-52 flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center">
+    <div style="background-image: url('/img/fleet/banner.jpg')" class="h-52 flex flex-col justify-center items-center bg-no-repeat bg-center">
         <h1 class="text-white text-center font-black text-6xl">@lang('Fleet-Banner-Title')</h1>
     </div>
 
     <!-- Fleet -->
     <div class="w-full">
         <input type="hidden" value="{{count($fleet)}}" id="fleetLength">
-        <iframe src="https://reservas.achieverentacar.com/pt/frota?iframe" scrolling="no" id="anyrentBookingsIframe" style="width: 1px; min-width: 100%; border: 0px; overflow: hidden;"></iframe>
+        <iframe src="https://reservas.achieverentacar.com/{{$locale}}/frota?iframe" scrolling="no" id="anyrentBookingsIframe" style="width: 1px; min-width: 100%; border: 0px; overflow: hidden;"></iframe>
     </div>
 
 @endsection
