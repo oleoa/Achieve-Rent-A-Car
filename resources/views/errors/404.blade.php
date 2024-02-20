df0a4a0f89f6b5bc35cc627fa2f2b8595d1f4b75
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-02-20 16:19:21 +0000

    removing the app.js 404 error in errors views

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Leonardo Abreu de Paulo">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite('resources/css/app.css')
     <title>Page not found</title>
   </head>
 
