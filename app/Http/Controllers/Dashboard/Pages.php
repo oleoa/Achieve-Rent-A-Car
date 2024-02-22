061fe8d19a28c0f7ffecdbd372586e5759ee8c6c
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-02-22 17:59:30 +0000

    improving pages

--- app/Http/Controllers/Dashboard/Pages.php
+++ app/Http/Controllers/Dashboard/Pages.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\Dashboard\Controller;
+use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 
 // Controller for translations and pages
@@ -10,6 +11,19 @@ class Pages extends Controller
 {
     public function index()
     {
+        $translations = array();
+        $translations['en'] = json_decode(File::get(base_path('resources/lang/en.json')));
+        $translations['pt'] = json_decode(File::get(base_path('resources/lang/pt.json')));
+
+        $pages = array(
+            'home' => 'Home',
+            'about' => 'About',
+            'fleet' => 'Fleet',
+            'seats' => 'Seats',
+            'faq' => 'FAQ',
+            'contact' => 'Contact'
+        );
+
         return $this->load('dashboard.pages.index', 'pages');
     }
 }
--- resources/views/dashboard/pages/index.blade.php
+++ resources/views/dashboard/pages/index.blade.php
@@ -12,6 +12,12 @@
             <a class="px-4 py-2 rounded bg-neutral-700 border-2" href="">About Us</a>
         </div>
 
+        <!-- Row to select the language -->
+        <div class="bg-neutral-800 flex flex-row gap-4 rounded p-4">
+            <a class="px-4 py-2 rounded bg-neutral-700 border-2" href="">Portuguese</a>
+            <a class="px-4 py-2 rounded bg-neutral-700 border-2" href="">English</a>
+        </div>
+
         <!-- Content -->
         <div class="grid grid-cols-2 h-full">
             
