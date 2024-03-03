a0504e07592dc419037fd5fba8fd9bea08aed11f
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-03-03 15:11:00 +0000

    banner to normal

--- resources/views/components/client/banner-carousel.blade.php
+++ resources/views/components/client/banner-carousel.blade.php
@@ -2,35 +2,23 @@
 <div class="relative z-10 w-full overflow-x-hidden xl:h-homeTitleXL h-homeTitle">
 
   <!-- Carousel Slide -->
-  <div id="carousel-slide-banner" class="rounded grid transition-transform duration-500 xl:h-homeTitleXL h-homeTitle w-full" style="grid-template-columns: @for($i = 1; $i <= 3; $i++) 100% @endfor">
+  <div id="carousel-slide-banner" class="rounded grid transition-transform duration-500 xl:h-homeTitleXL h-homeTitle w-full" style="grid-template-columns: @for($i = 1; $i <= 1; $i++) 100% @endfor">
       
     <!-- Carousel Item 1 -->
-    <div class="w-full xl:h-homeTitleXL h-homeTitle relative bg-cover bg-center flex items-center justify-center" style="background-image: url(/img/about/2.jpeg)">
+    <div class="w-full xl:h-homeTitleXL h-homeTitle relative bg-cover bg-center grid grid-cols-2 content-center xl:px-margin px-4" style="background-image: url(/img/home/banner.jpg)">
 
-      <h1 itemprop="https://schema.org/name" class="text-flagRed p-4 rounded-xl bg-white/75 font-black xl:text-4xl text-lg h-fit w-fit">@lang('home-Banner-Title')</h1>
-
-    </div>
-      
-    <!-- Carousel Item 1 -->
-    <div class="w-full xl:h-homeTitleXL h-homeTitle relative bg-cover bg-center flex items-center justify-center" style="background-image: url(/img/about/2.jpeg)">
-
-      <h1 itemprop="https://schema.org/name" class="text-flagRed p-4 rounded-xl bg-white/75 font-black xl:text-4xl text-lg h-fit w-fit">@lang('home-Banner-Title')</h1>
-
-    </div>
-      
-    <!-- Carousel Item 1 -->
-    <div class="w-full xl:h-homeTitleXL h-homeTitle relative bg-cover bg-center flex items-center justify-center" style="background-image: url(/img/about/2.jpeg)">
-
-      <h1 itemprop="https://schema.org/name" class="text-flagRed p-4 rounded-xl bg-white/75 font-black xl:text-4xl text-lg h-fit w-fit">@lang('home-Banner-Title')</h1>
+      <h1 itemprop="https://schema.org/name" class="text-flagRed p-4 rounded-xl bg-white/75 font-black xl:text-4xl text-lg h-fit w-fit">
+        @lang('home-Banner-Title')
+      </h1>
 
     </div>
 
   </div>
 
   <!-- Carousel Indicator -->
-  <div class="absolute bottom-0 w-full flex justify-center items-center gap-2 p-4 xl:z-auto z-40">
+  <div class="hidden absolute bottom-0 w-full {{--flex--}} justify-center items-center gap-2 p-4 xl:z-auto z-40">
   
-    @for ($i = 0; $i <= 2; $i++)
+    @for ($i = 0; $i <= 0; $i++)
     
       <div class="w-4 h-4 bg-white rounded-full cursor-pointer" id="carousel-{{$i}}-indicator-banner"></div>
 
