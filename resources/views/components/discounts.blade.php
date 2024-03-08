88c07a7008041b3dba8b1da443152f220229bf2b
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-03-08 09:32:04 +0000

    lang in discounts so <br> works

--- resources/views/components/discounts.blade.php
+++ resources/views/components/discounts.blade.php
@@ -2,9 +2,9 @@
   <div class="text-sm xl:text-xl w-full min-h-navbar py-4 xl:px-margin px-4 flex justify-center items-center" style="background-color: {{$discount['color']}}; color: {{$discount['text_color']}};">
     <p class="w-full h-full text-center">
       @if ($locale=='en')
-        {{$discount['text']}}
-      @elseif ($locale=='pt')
-        {{$discount['texto']}}
+          @lang($discount['text'])
+        @elseif ($locale=='pt')
+          @lang($discount['texto'])
       @endif
     </p>
   </div>
