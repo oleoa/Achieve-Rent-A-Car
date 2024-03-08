934af7c8943cfa661acea43cbf8e7fc9f6b4c852
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-03-08 09:27:39 +0000

    discounts text center always

--- resources/views/components/discounts.blade.php
+++ resources/views/components/discounts.blade.php
@@ -1,11 +1,11 @@
 @if($discount)
-    <div class="text-sm xl:text-xl w-full min-h-navbar py-4 xl:px-margin px-4" style="background-color: {{$discount['color']}}; color: {{$discount['text_color']}};">
-        <p>
-            @if ($locale=='en')
-                {{$discount['text']}}
-            @elseif ($locale=='pt')
-                {{$discount['texto']}}
-            @endif
-        </p>
-    </div>
+  <div class="text-sm xl:text-xl w-full min-h-navbar py-4 xl:px-margin px-4 flex justify-center items-center" style="background-color: {{$discount['color']}}; color: {{$discount['text_color']}};">
+    <p class="w-full h-full text-center">
+      @if ($locale=='en')
+        {{$discount['text']}}
+      @elseif ($locale=='pt')
+        {{$discount['texto']}}
+      @endif
+    </p>
+  </div>
 @endif
