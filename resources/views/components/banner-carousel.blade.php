9b4649b4312f691bbbf28c99fd9d91754cf0f2d2
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-03-08 20:03:03 +0000

    banner for computer

--- resources/views/components/banner-carousel.blade.php
+++ resources/views/components/banner-carousel.blade.php
@@ -7,35 +7,38 @@
   <div id="carousel-slide-banner" class="rounded xl:grid hidden transition-transform duration-500 w-full" style="grid-template-columns: @for($i = 1; $i <= $slides; $i++) 100% @endfor">
     
     <!-- Carousel Item 1 -->
-    <div class="w-full flex flex-col items-center justify-center h-homeTitleXL bg-cover bg-bottom" style="background-image: url('/img/home/banner1.png')">
+    <div class="w-full flex items-center justify-start px-margin h-homeTitleXL bg-cover bg-bottom" style="background-image: url('/img/home/banner1.png')">
 
-      <h2 class="text-white font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,100)]">Achieve Rent A Car</h2>
-
-      <h1 class="text-white font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,100)]">Rent a Car na Ilha da Madeira</h1>
-
-      <p class="text-white font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,100)]">A melhor relação Preço / Qualidade para alugar um carro na Ilha da Madeira</p>
+      <div class="bg-white/70 text-flagRed px-4 py-2 rounded-md font-semibold text-2xl">
+        <h1>Rent a car - Ilha da Madeira</h1>
+        <p>A melhor relação preço qualidade.</p>
+        <p>Sem depósito, sem franquia.</p>
+        <p>Quilometragem ilimitada.</p>
+        <p>Sem cartão de crédito.</p>
+      </div>
       
     </div>
     
     <!-- Carousel Item 2 -->
-    <div class="w-full flex flex-col items-center justify-center h-homeTitleXL bg-cover bg-center" style="background-image: url('/img/home/banner2.png')">
+    <div class="w-full flex items-center justify-end px-margin h-homeTitleXL bg-cover bg-center" style="background-image: url('/img/home/banner2.png')">
 
-      <h2 class="text-white font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,100)]">Achieve Rent A Car</h2>
-
-      <h1 class="text-white font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,100)]">Rent a Car na Ilha da Madeira</h1>
-
-      <p class="text-white font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,100)]">Seguro Premium incluído. Sem depósito, sem franquia. Sem cartão de crédito.</p>
+      <div class="bg-white/70 text-flagRed px-4 py-2 rounded-md font-semibold text-2xl">
+        <h1>Rent a car - Ilha da Madeira</h1>
+        <p>Condutor adicional gratuito.</p>
+        <p>Seguro Premium incluído.</p>
+      </div>
 
     </div>
     
     <!-- Carousel Item 3 -->
-    <div class="w-full flex flex-col items-center justify-center h-homeTitleXL bg-cover bg-center" style="background-image: url('/img/home/banner3.png')">
+    <div class="w-full flex items-center justify-start pl-margin h-homeTitleXL bg-cover bg-center" style="background-image: url('/img/home/banner3.png')">
 
-      <h2 class="text-white font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,100)]">Achieve Rent A Car</h2>
-
-      <h1 class="text-white font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,100)]">Rent a Car na Ilha da Madeira</h1>
-
-      <p class="text-white font-semibold drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,100)]">Receba e devolva onde preferir. Atendimento altamente recomendado. Quilometragem ilimitada.</p>
+      <div class="bg-white/70 text-flagRed px-4 py-2 rounded-md font-semibold text-2xl">
+        <h1>Rent a car - Ilha da Madeira</h1>
+        <p>Receba e devolva onde preferir.</p>
+        <p>Contratação rápida e fácil.</p>
+        <p>Atendimento 5 estrelas.</p>
+      </div>
 
     </div>
 
