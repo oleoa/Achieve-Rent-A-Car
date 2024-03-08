ca0901a51c06adcdc842ce3f80cb21b835d1ceaf
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-03-08 10:09:32 +0000

    discounts for computer and mobile

--- resources/views/components/discounts.blade.php
+++ resources/views/components/discounts.blade.php
@@ -1,10 +1,17 @@
 @if($discount)
   <div class="text-sm xl:text-xl w-full min-h-navbar py-4 xl:px-margin px-4 flex justify-center items-center" style="background-color: {{$discount['color']}}; color: {{$discount['text_color']}};">
-    <p class="w-full h-full text-center">
+    <p class="w-full h-full text-center xl:block hidden">
       @if ($locale=='en')
-          @lang($discount['text'])
-        @elseif ($locale=='pt')
-          @lang($discount['texto'])
+        @lang($discount['text'])
+      @elseif ($locale=='pt')
+        @lang($discount['texto'])
+      @endif
+    </p>
+    <p class="w-full h-full text-center xl:hidden block">
+      @if ($locale=='en')
+        @lang($discount['text_mobile'])
+      @elseif ($locale=='pt')
+        @lang($discount['texto_mobile'])
       @endif
     </p>
   </div>
