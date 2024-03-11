aab8704144b0e90204522b76e7e60652c2bc6e52
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-03-11 11:52:15 +0000

    style in the head

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -2,6 +2,28 @@
 <html>
   <head>
     <title>Novo Contato Do Site Achieve</title>
+    <style>
+
+      body {
+        font-family: Arial, sans-serif;
+        background-color: #161616;
+        padding-right: 4rem;
+        padding-left: 4rem;
+        padding-top: 4rem;
+        padding-bottom: 4rem;
+        color: #fff;
+      }
+  
+      @media (min-width: 1280px) {
+        body {
+          padding-left: 15rem;
+          padding-right: 15rem;
+          padding-top: 4rem;
+          padding-bottom: 4rem;
+        }
+      }
+  
+    </style>
   </head>
   <body>
     <h1>Novo Contato Do Site Achieve</h1>
@@ -10,26 +32,4 @@
     <p><strong>Phone:</strong> {{ $data['phone'] }}</p>
     <p><strong>Message:</strong> {{ $data['message'] }}</p>
   </body>
-  <style>
-
-    body {
-      font-family: Arial, sans-serif;
-      background-color: #161616;
-      padding-right: 4rem;
-      padding-left: 4rem;
-      padding-top: 4rem;
-      padding-bottom: 4rem;
-      color: #fff;
-    }
-
-    @media (min-width: 1280px) {
-      body {
-        padding-left: 15rem;
-        padding-right: 15rem;
-        padding-top: 4rem;
-        padding-bottom: 4rem;
-      }
-    }
-
-  </style>
 </html>
