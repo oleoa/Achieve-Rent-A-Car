5be2bf5bdc50a553988ca6fd08c8051b46565cda
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-03-24 16:23:40 +0000

    seats

---
+++ resources/lang/en/seats.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+  'banner' => 'Car Seats',
+  'title' => 'Car Seats',
+  'paragraph' => 'We at Achieve Rent a Car want our customers to have a fantastic experience when visiting our Island. <br> Safety for everyone is paramount to us! And this becomes even more serious when it comes to the safety of our little customers, who are your babies and your children! Therefore, we offer you the best car seats on the market. Below, you can identify the model and group that best suits your needs.',
+  'seats.title' => 'Choose the Right Car Seat for Your Child',
+  'seats.card.1.title' => 'Groups 1 & 2 - Car Seat',
+  'seats.card.1.paragraph' => 'The groups 1 & 2 - Car Seat are suitable for children weighing between 09 kg and 25 kg.',
+  'seats.card.2.title' => 'Group 3 - Booster Car Seat',
+  'seats.card.2.paragraph' => 'The group 3 - Booster Car Seat is suitable for children weighing between 22 kg and 36 kg.',
+];
--- resources/views/seats.blade.php
+++ resources/views/seats.blade.php
@@ -1,12 +1,10 @@
 @extends('layouts.main')
-@php
-  $title = 'seats.title';
-@endphp
+@php $title = 'title.seats'; @endphp
 @section('main')
 
   <!-- Banner -->
-  <div style="background-image: url('/img/seats/banner.jpg')" class="h-52 flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center">
-    <h1 class="text-white text-center font-black text-6xl">@lang('Seats-Banner-Title')</h1>
+  <div style="background-image: url('/img/pages/seats/banner.jpg')" class="h-52 flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center">
+    <h1 class="text-white text-center font-black text-6xl">@lang('seats.banner')</h1>
   </div>
 
   <!-- Intro -->
@@ -19,15 +17,15 @@
       <div class="flex flex-col md:items-start items-center justify-center gap-4 order-1">
 
         <!-- Title -->
-        <h2 class="text-center md:text-start py-0 md:text-4xl text-xl font-bold">@lang('Seats-1-Title')</h2>
+        <h2 class="text-center md:text-start py-0 md:text-4xl text-xl font-bold">@lang('seats.title')</h2>
 
         <!-- Paragraph -->
-        <p class="text-justify md:text-start">@lang('Seats-1-Paragraph')</p>
+        <p class="text-justify md:text-start">@lang('seats.paragraph')</p>
 
       </div>
 
       <!-- Image -->
-      <img src="{{asset('/img/seats/main.jpg')}}" alt="@lang('Madeira.Island.Image.Alt')" class="rounded-lg order-2 w-full">
+      <img src="{{asset('/img/pages/seats/main.jpg')}}" alt="@lang('Madeira.Island.Image.Alt')" class="rounded-lg order-2 w-full">
 
     </div>
 
@@ -45,7 +43,7 @@
   <div class="py-4 flex flex-col gap-8">
           
     <!-- Title -->
-    <h2 class="py-4 text-center font-bold text-4xl">@lang('Seats-2-Title')</h2>
+    <h2 class="py-4 text-center font-bold text-4xl">@lang('seats.seats.title')</h2>
 
     <!-- Cards -->
     <div class="grid md:grid-cols-2 w-full xl:px-margin px-4 gap-8 justify-items-center">
@@ -57,16 +55,16 @@
         <div class="rounded-lg flex flex-col items-center w-96 text-center">
 
           <!-- Image -->
-          <img src="{{asset('/img/seats/'.$i.'.png')}}" alt="@lang('Madeira.Island.Image.Alt')" class="rounded-t-lg w-96">
+          <img src="{{asset('/img/pages/seats/'.$i.'.png')}}" alt="@lang('Madeira.Island.Image.Alt')" class="rounded-t-lg w-96">
 
           <div class="p-4">
 
             <!-- Title -->
-            <h3 class="text-xl font-bold">@lang('Seats-2-Card.'.$i.'-Title')</h3>
+            <h3 class="text-xl font-bold">@lang('seats.seats.card.'.$i.'.title')</h3>
 
             <!-- Paragraph -->
             <p class="py-2 text-base text-center">
-              @lang('Seats-2-Card.'.$i.'-Paragraph')
+              @lang('seats.seats.card.'.$i.'.paragraph')
             </p>
 
           </div>
