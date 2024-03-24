24a89e363614a4ebfde1cbd8e039ed98cf8cd9b3
Leonardo Abreu <leonardo.abreu.de.paulo@gmail.com>
2024-03-24 20:40:29 +0000

    reviews in json

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 
 class Home extends Controller
@@ -13,6 +14,16 @@ class Home extends Controller
     $this->isCurrent('home');
     $this->setDiscount();
     $this->createView();
+
+    $pathToReviews = public_path('reviews.json');
+    if (File::exists($pathToReviews)) {
+      $jsonReviews = File::get($pathToReviews);
+      $arrayReviews = json_decode($jsonReviews, true);
+      $this->data['reviews'] = $arrayReviews;
+    } else {
+      $this->data['reviews'] = [];
+    }
+
     return view('home', $this->data);
   }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,117 @@
     </article>
 
     <span id="home-page"></span>
-    <x-reviews/>
+    
+    <!-- Reviews Computer -->
+    <div class="relative z-10 w-full overflow-x-hidden xl:block hidden">
+    
+      <!-- Carousel Slide -->
+      <div id="carousel-slide-reviews" class="grid transition-transform duration-500 w-full md:margin px-4 pb-8" style="grid-template-columns: @for($i = 1; $i <= count($reviews); $i++) calc(100%/3) @endfor">
+        
+        @for ($i = 1; $i <= count($reviews); $i++)
+        
+          <!-- Carousel Item -->
+          <div class="reviews-item w-full p-4 cursor-pointer" id="{{$i-1}}-reviews">
+    
+            <div class="flex flex-col justify-start items-center h-full p-12 gap-4 rounded-lg shadow-2xl bg-white">
+  
+              <div class="flex w-full gap-8 items-start">
+                
+                <img src="{{$reviews[$i]['photo']}}" alt="Valentina Vucicic" class="h-20 w-20">
+                
+                <div class="flex flex-col gap-2">
+  
+                  <h2 class="font-bold">{{$reviews[$i]['name']}}</h2>
+          
+                  <h3 class="[&>i]:text-yellow-400 text-base"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></h3>              
+  
+                </div>
+  
+              </div>
+  
+              <p class="font-semibold">{{$reviews[$i]['comment'][$language]}}</p>
+    
+            </div>
+    
+          </div>
+        
+        @endfor
+    
+      </div>
+    
+      <!-- Carousel Indicator -->
+      <div class="absolute bottom-0 w-full hidden justify-center items-center gap-2 p-4 md:z-auto z-40" id="carousel-indicators-div-reviews">
+      
+        <input type="hidden" id="carousel-selected-indicator-color-reviews" value="bg-white">
+    
+        @for ($i = 0; $i <= count($reviews)-1; $i++)
+        
+          <div class="w-4 h-4 rounded-full cursor-pointer border-2 border-white" id="carousel-{{$i}}-indicator-reviews"></div>
+    
+        @endfor
+    
+      </div>
+        
+    </div>
+    
+    <!-- Reviews Mobile/Tablet -->
+    <div class="relative z-10 w-full overflow-x-hidden xl:hidden">
+    
+      <!-- Carousel Slide -->
+      <div id="carousel-slide-reviews-mobile" class="grid transition-transform duration-500 w-full py-8" style="grid-template-columns: @for($i = 1; $i <= count($reviews); $i++) 100% @endfor">
+      
+        @for ($i = 1; $i <= count($reviews); $i++)
+            
+          <!-- Carousel Item -->
+          <div class="reviews-mobile-item w-full p-4" id="{{$i-1}}-reviews-mobile">
+    
+            <div class="flex flex-col justify-start items-center h-full p-12 gap-4 rounded-md shadow-2xl">
+    
+              <img src="@lang('home-new-reviews-'.$i.'-image')" alt="Valentina Vucicic" class="h-20 w-20">
+      
+              <h2 class="font-bold">@lang('home-new-reviews-'.$i.'-author')</h2>
+      
+              <h3 class="[&>i]:text-yellow-400 text-base"><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i></h3>
+      
+              <p>@lang('home-new-reviews-'.$i.'-comment')</p>
+    
+            </div>
+    
+          </div>
+          
+        @endfor
+    
+      </div>
+    
+      <!-- Carousel Buttons -->
+      <div class="absolute top-0 w-full h-full hidden justify-between md:items-center items-end md:z-auto z-50" id="carousel-buttons-div-reviews-mobile">
+            
+        <!-- Previous Button -->
+        <button class="cursor-pointer w-32 flex items-center justify-center" id="prevBtn-reviews-mobile">
+          <i class="fas fa-arrow-left p-4 md:h-auto bg-flagRed h-16 w-16 text-4xl rounded-xl text-white"></i>
+        </button>
+        
+        <!-- Next Button -->
+        <button class="cursor-pointer w-32 flex items-center justify-center" id="nextBtn-reviews-mobile">
+          <i class="fas fa-arrow-right p-4 md:h-auto bg-flagRed h-16 w-16 text-4xl rounded-xl text-white"></i>
+        </button>
+    
+      </div>
+    
+      <!-- Carousel Indicator -->
+      <div class="absolute bottom-0 w-full hidden justify-center items-center gap-2 p-4 md:z-auto z-40" id="carousel-indicators-div-reviews-mobile">
+      
+        <input type="hidden" id="carousel-selected-indicator-color-reviews-mobile" value="bg-flagBlue">
+    
+        @for ($i = 0; $i <= count($reviews)-1; $i++)
+        
+          <div class="w-4 h-4 rounded-full cursor-pointer border-2 border-flagBlue" id="carousel-{{$i}}-indicator-reviews-mobile"></div>
+    
+        @endfor
+    
+      </div>
+        
+    </div> 
 
   </div>
 
