96bf01bbc219420b61c599c47a2141d29afc6ae2
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-03-29 11:20:05 +0000

    restaurants recommended

--- app/Http/Controllers/Recomendations.php
+++ app/Http/Controllers/Recomendations.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 
 class Recomendations extends Controller
@@ -12,6 +13,16 @@ class Recomendations extends Controller
         $this->isCurrent('recomendations');
         $this->setDiscount();
         $this->createView();
+
+        $pathToRecomendations = public_path('recomendations.json');
+        if (File::exists($pathToRecomendations)) {
+            $json = File::get($pathToRecomendations);
+            $array = json_decode($json, true);
+            $this->data['recomendations'] = $array;
+        } else {
+            $this->data['recomendations'] = [];
+        }
+
         return view('recomendations', $this->data);
     }
 }
--- resources/views/recomendations.blade.php
+++ resources/views/recomendations.blade.php
@@ -1,28 +1,29 @@
 @extends('layouts.main')
-@php $title = 'title.about'; @endphp
+@php $title = 'title.recomendations'; @endphp
 @section('main')
 
-  <div style="background-image: url('/img/pages/about/2.jpeg')" class="h-52 flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center">
-    <h1 class="text-white text-center font-black text-6xl">@lang('about.banner')</h1>
+  <div style="background-image: url('/img/pages/recomendations/1.jpg')" class="h-52 flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center">
+    <h1 class="text-white text-center font-black text-6xl">@lang('recomendations.banner')</h1>
   </div>
+  
+  <article class="grid grid-cols-3 gap-4 py-4 2xl:px-64 lg:px-20 px-4">
 
-  <!-- Content -->
-  <div class="w-full md:px-64 px-4 grid md:grid-cols-5 py-4 gap-4">
+    @foreach ($recomendations as $r)
+    
+      <div class="rounded-md shadow-2xl w-full flex flex-col gap-2 pb-2 items-start">
+        <img src="{{asset($r['img'])}}" alt="{{$r['name']}} Image" class="rounded-t-md w-full h-32 object-cover">
+        <div class="flex flex-col gap-2 justify-between h-full px-2">
+          <h2>{{$r['name']}}</h2>
+          <p class="text-start">{{$r['description']}}</p>
+          <div class="flex gap-2">
+            <a class="px-4 py-2 rounded-md bg-flagRed/70 text-white w-fit hover:text-white hover:bg-flagRed cursor-pointer">Saiba Mais</a>
+            <a class="px-4 py-2 rounded-md bg-flagRed/70 text-white w-fit hover:text-white hover:bg-flagRed cursor-pointer">Achar no mapa</a>
+          </div>
+        </div>
+      </div>
 
-    <!-- Text -->
-    <div class="flex flex-col gap-4 md:col-span-2">
+    @endforeach
 
-      <!-- Title -->
-      <h2>@lang('about.title')</h2>
-
-      <!-- Paragraph -->
-      <p>@lang('about.paragraph')</p>
-
-    </div>
-
-    <!-- Image -->
-    <img src="{{url('/img/pages/about/1.jpg')}}" alt="@lang('Madeira.Island.Image.Alt')" class="rounded-xl md:col-span-3">
-
-  </div>
+  </article>
 
 @endsection
