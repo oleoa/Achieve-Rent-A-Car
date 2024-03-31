5cf5aa707ed084ff2bb55d591213f77c91e17a43
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-03-31 16:27:48 +0100

    only paragraph is missing

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,23 +6,21 @@
     <h1 class="text-white text-center font-black text-6xl">@lang('about.banner')</h1>
   </div>
 
-  <!-- Content -->
-  <div class="w-full md:px-64 px-4 grid md:grid-cols-5 py-4 gap-4">
+  <div class="w-full md:px-64 px-4 flex flex-col gap-4 pb-12">
 
-    <!-- Text -->
-    <div class="flex flex-col gap-4 md:col-span-2">
+    @for ($i = 1; $i <= 3; $i++)
+        
+      <div class="grid md:grid-cols-5 py-4 gap-8">
+        <div @class([
+          "flex flex-col gap-4 md:col-span-3",
+          "order-2" => ($i % 2 == 0),
+        ])>
+          <h2 class="font-semibold">@lang('about.item.'.$i.'.title')</h2>
+          <p>@lang('about.item.'.$i.'.paragraph')</p>
+        </div>
+        <img src="{{url('/img/pages/about/'.$i.'.jpg')}}" alt="@lang('Madeira.Island.Image.Alt')" class="rounded-md h-full object-cover md:col-span-2">
+      </div>
 
-      <!-- Title -->
-      <h2>@lang('about.title')</h2>
-
-      <!-- Paragraph -->
-      <p>@lang('about.paragraph')</p>
-
-    </div>
-
-    <!-- Image -->
-    <img src="{{url('/img/pages/about/1.jpg')}}" alt="@lang('Madeira.Island.Image.Alt')" class="rounded-xl md:col-span-3">
-
-  </div>
-
+    @endfor
+    
 @endsection
