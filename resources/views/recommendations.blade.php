963800ff9ad8e2515fd6b4d2d6f19901a78ed2ad
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-03-31 13:11:37 +0100

    to fininsh recommendations

--- resources/views/recommendations.blade.php
+++ resources/views/recommendations.blade.php
@@ -9,19 +9,17 @@
   <article class="grid grid-cols-3 gap-4 py-4 2xl:px-64 lg:px-20 px-4">
 
     @foreach ($recommendations as $r)
-    
-      <div class="rounded-md shadow-2xl w-full flex flex-col gap-4 pb-4 items-start">
-        <img src="{{asset($r['img'])}}" alt="{{$r['name']}} Image" class="rounded-t-md w-full h-32 object-cover">
-        <div class="flex flex-col gap-4 justify-between h-full px-4">
-          <h2>{{$r['name']}}</h2>
-          <p class="text-start">{{$r['description']}}</p>
-          <div class="flex gap-4">
-            <a class="px-4 py-2 rounded-md bg-flagRed/70 text-white w-fit hover:text-white hover:bg-flagRed cursor-pointer">Saiba Mais</a>
-            <a class="px-4 py-2 rounded-md bg-flagRed/70 text-white w-fit hover:text-white hover:bg-flagRed cursor-pointer">Achar no mapa</a>
+      @if ($r['active'])
+        <div class="rounded-md shadow-2xl w-full flex flex-col gap-4 pb-4 items-start">
+          <img src="{{asset($r['img'])}}" alt="{{$r['name']}} Image" class="rounded-t-md w-full h-32 object-cover">
+          <div class="flex flex-col gap-4 justify-between h-full px-4">
+            <h2>{{$r['name']}}</h2>
+            <p class="text-start">{{$r['description']}}</p>
+            <a target="_blank" href="{{$r['local']}}" class="px-4 py-2 flex items-center gap-2 transition-all duration-300 rounded-md bg-flagBlue text-white w-fit hover:text-flagBlue hover:bg-white border-2 border-flagBlue cursor-pointer"><i class="fa-solid fa-map-location-dot"></i>Achar no mapa</a>
+            <a target="_blank" href="{{$r['website']}}" class="px-4 py-2 flex items-center gap-2 transition-all duration-300 rounded-md bg-flagRed text-white w-fit hover:text-flagRed hover:bg-white border-2 border-flagRed cursor-pointer"><i class="fa-solid fa-circle-info"></i>Saiba Mais</a>
           </div>
         </div>
-      </div>
-
+      @endif
     @endforeach
 
   </article>
