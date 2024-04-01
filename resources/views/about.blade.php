6b169430f5da57d29148d6482e7ce29a3830d58b
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-04-01 14:36:07 +0100

    about us page done

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,15 +10,15 @@
 
     @for ($i = 1; $i <= 3; $i++)
         
-      <div class="grid md:grid-cols-5 py-4 gap-8">
+      <div class="grid xl:grid-cols-5 py-4 gap-8 justify-center">
         <div @class([
-          "flex flex-col gap-4 md:col-span-3",
-          "order-2" => ($i % 2 == 0),
+          "flex flex-col gap-4 xl:col-span-3",
+          "xl:order-2" => ($i % 2 == 0),
         ])>
           <h2 class="font-semibold">@lang('about.item.'.$i.'.title')</h2>
           <p>@lang('about.item.'.$i.'.paragraph')</p>
         </div>
-        <img src="{{url('/img/pages/about/'.$i.'.jpg')}}" alt="@lang('Madeira.Island.Image.Alt')" class="rounded-md h-full object-cover md:col-span-2">
+        <img src="{{url('/img/pages/about/'.$i.'.jpg')}}" alt="@lang('Madeira.Island.Image.Alt')" class="rounded-xl h-full object-cover xl:col-span-2">
       </div>
 
     @endfor
