a95602ed840ea0fdf9833e38d13c2a5cabac4e02
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-04-08 15:42:57 +0100

    new paddings

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,87 +1,87 @@
 <div id="headerContent" @class([
-    "w-full bg-white flex items-center lg:justify-between justify-center gap-4 p-4 px-default relative",
+    "w-full bg-white flex items-center xl:justify-between justify-center gap-4 p-4 px-default relative",
     "[&>a]:flex [&>a]:items-center [&>a]:justify-center text-flagRed/40 hover:[&>a]:text-flagRed [&>a]:h-fit",
 ])>
 
     <div class="absolute left-0 top-0 w-full h-full px-8 flex items-center">
-        <button title="@lang('menu.toggle.title')" class="lg:hidden sidebar-toggle">
+        <button title="@lang('menu.toggle.title')" class="xl:hidden sidebar-toggle">
             <i class="fas fa-bars text-flagRed text-xl"></i>
         </button>
     </div>
 
-    <img src="{{asset('/img/navbar/phone.webp')}}" alt="Achieve Rent a Car Log" height="48" width="213" class="block lg:hidden max-h-12 lg:px-0 px-14 object-cover">
-    <img src="{{asset('/img/navbar/computer.webp')}}" alt="Achieve Rent a Car Log" height="48" width="306" class="hidden lg:block max-h-12 lg:px-0 px-14 object-cover">
+    <img src="{{asset('/img/navbar/phone.webp')}}" alt="Achieve Rent a Car Log" height="48" width="213" class="block xl:hidden max-h-12 xl:px-0 px-14 object-cover">
+    <img src="{{asset('/img/navbar/computer.webp')}}" alt="Achieve Rent a Car Log" height="48" width="306" class="hidden xl:block max-h-12 xl:px-0 px-14 object-cover">
     
-    <div id="sidebar" class="lg:static fixed z-50 lg:flex lg:items-center lg:justify-end grid grid-cols-2 gap-0 top-0 left-0 w-full h-full transition-transform duration-300 transform -translate-x-full lg:-translate-x-0">
+    <div id="sidebar" class="xl:static fixed z-50 xl:flex xl:items-center xl:justify-end grid grid-cols-2 gap-0 top-0 left-0 w-full h-full transition-transform duration-300 transform -translate-x-full xl:-translate-x-0">
 
         <div @class([
             "flex gap-6", {{-- Both --}}
-            "[&>a]:flex [&>a]:items-center [&>a]:justify-center text-white/50 lg:text-flagRed lg:hover:[&>a]:text-flagRed lg:hover:[&>a]:underline hover:[&>a]:text-white [&>a]:h-fit", {{-- Children --}}
-            "lg:flex-row lg:bg-white lg:p-0 lg:h-full lg:items-center lg:w-full lg:static lg:w-fit lg:justify-end", {{-- Computer --}}
+            "[&>a]:flex [&>a]:items-center [&>a]:justify-center text-white/50 xl:text-flagRed xl:hover:[&>a]:text-flagRed xl:hover:[&>a]:underline hover:[&>a]:text-white [&>a]:h-fit", {{-- Children --}}
+            "xl:flex-row xl:bg-white xl:p-0 xl:h-full xl:items-center xl:w-full xl:static xl:w-fit xl:justify-end", {{-- Computer --}}
             "flex-col p-4 bg-flagRed items-start left-0 top-0 h-full relative", {{-- Phone --}}
         ])>
 
-            <button title="@lang('menu.toggle.title')" class="absolute right-0 top-0 p-4 lg:hidden sidebar-toggle">
+            <button title="@lang('menu.toggle.title')" class="absolute right-0 top-0 p-4 xl:hidden sidebar-toggle">
                 <i class="fas fa-xmark text-white text-xl"></i>
             </button>
 
             <a title="@lang('menu.home.title')" href="{{route('home', ['locale' => $locale])}}" @class([
                 "flex gap-2",
-                "lg:text-flagRed text-white underline" => $current == 'home',
+                "xl:text-flagRed text-white underline" => $current == 'home',
             ])>
-                <i class="fa-solid fa-house lg:min-w-min min-w-8 flex items-center justify-start"></i>
+                <i class="fa-solid fa-house xl:min-w-min min-w-8 flex items-center justify-start"></i>
                 @lang('menu.home')
             </a>
 
             <a title="@lang('menu.fleet.title')" href="{{route('fleet', ['locale' => $locale])}}" @class([
                 "flex gap-2",
-                "lg:text-flagRed text-white underline" => $current == 'fleet',
+                "xl:text-flagRed text-white underline" => $current == 'fleet',
             ])>
-                <i class="fa-solid fa-car lg:min-w-min min-w-8 flex items-center justify-start"></i>
+                <i class="fa-solid fa-car xl:min-w-min min-w-8 flex items-center justify-start"></i>
                 @lang('menu.fleet')
             </a>
             
             <a title="@lang('menu.seats.title')" href="{{route('seats', ['locale' => $locale])}}" @class([
                 "flex gap-2",
-                "lg:text-flagRed text-white underline" => $current == 'seats',
+                "xl:text-flagRed text-white underline" => $current == 'seats',
             ])>
-                <i class="fa-solid fa-baby lg:min-w-min min-w-8 flex items-center justify-start"></i>
+                <i class="fa-solid fa-baby xl:min-w-min min-w-8 flex items-center justify-start"></i>
                 @lang('menu.seats')
             </a>
             
             <a title="@lang('menu.stays.title')" href="{{route('stays', ['locale' => $locale])}}" @class([
                 "flex gap-2",
-                "lg:text-flagRed text-white underline" => $current == 'stays',
+                "xl:text-flagRed text-white underline" => $current == 'stays',
             ])>
-                <i class="fa-solid fa-bed lg:min-w-min min-w-8 flex items-center justify-start"></i>
+                <i class="fa-solid fa-bed xl:min-w-min min-w-8 flex items-center justify-start"></i>
                 @lang('menu.stays')
             </a>
             
             <a title="@lang('menu.about.title')" href="{{route('about', ['locale' => $locale])}}" @class([
                 "flex gap-2",
-                "lg:text-flagRed text-white underline" => $current == 'about',
+                "xl:text-flagRed text-white underline" => $current == 'about',
             ])>
-                <i class="fa-solid fa-people-group lg:min-w-min min-w-8 flex items-center justify-start"></i>
+                <i class="fa-solid fa-people-group xl:min-w-min min-w-8 flex items-center justify-start"></i>
                 @lang('menu.about')
             </a>
             
             <a title="@lang('menu.faq.title')" href="{{route('faq', ['locale' => $locale])}}" @class([
                 "flex gap-2",
-                "lg:text-flagRed text-white underline" => $current == 'faq',
+                "xl:text-flagRed text-white underline" => $current == 'faq',
             ])>
-                <i class="fa-solid fa-circle-question lg:min-w-min min-w-8 flex items-center justify-start"></i>
+                <i class="fa-solid fa-circle-question xl:min-w-min min-w-8 flex items-center justify-start"></i>
                 @lang('menu.faq')
             </a>
             
             <a title="@lang('menu.contact.title')" href="{{route('contact', ['locale' => $locale])}}" @class([
                 "flex gap-2",
-                "lg:text-flagRed text-white underline" => $current == 'contact',
+                "xl:text-flagRed text-white underline" => $current == 'contact',
             ])>
-                <i class="fa-solid fa-envelope-open-text lg:min-w-min min-w-8 flex items-center justify-start"></i>
+                <i class="fa-solid fa-envelope-open-text xl:min-w-min min-w-8 flex items-center justify-start"></i>
                 @lang('menu.contact')
             </a>
 
-            <div class="flex flex-row justify-start lg:p-0 lg:mt-0 mt-auto lg:w-fit w-full rounded-md">
+            <div class="flex flex-row justify-start xl:p-0 xl:mt-0 mt-auto xl:w-fit w-full rounded-md">
 
                 <a title="@lang('menu.en.title')" href="{{route($current, ['locale' => ($locale == 'locale' || $locale == 'local') ? 'locale' : 'en'])}}" class="flex gap-2 items-center bg-white p-2 rounded-l-md">
                     <img src="{{asset('/img/flags/en.jpg')}}" alt="England Flag" height="24" width="32" class="object-cover rounded">
