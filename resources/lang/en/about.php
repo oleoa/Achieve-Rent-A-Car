8840ade731a9b28994f880b56578a13cd7073b6d
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-04-14 12:11:49 +0100

    achieve not family

--- resources/lang/en/about.php
+++ resources/lang/en/about.php
@@ -4,7 +4,7 @@ return [
   'banner' => 'About Us',
 
   'item.1.title' => 'We are a local company',
-  'item.1.paragraph' => 'When renting a car in Madeira, count on a local, family-owned company with a highly motivated team dedicated to welcoming and assisting you. We provide personalized, friendly, and flexible service from the moment you make your booking until you return the car. We are based in Funchal and for your comfort we deliver and collect the car at the location of your choice.',
+  'item.1.paragraph' => 'When renting a car in Madeira, count on a local company with a highly motivated team dedicated to welcoming and assisting you. We provide personalized, friendly, and flexible service from the moment you make your booking until you return the car. We are based in Funchal and for your comfort we deliver and collect the car at the location of your choice.',
   
   'item.2.title' => 'Our focus is your satisfaction',
   'item.2.paragraph' => 'We are people just like you: We like to be well attended to, with care. That\'s why we work this way: With attention to detail so that you feel welcome and well taken care of. After a quick and uncomplicated booking, we will send you all the information you need to feel safe and oriented. Your satisfaction is our priority. We will do our best to make sure you have an unforgettable experience.'
