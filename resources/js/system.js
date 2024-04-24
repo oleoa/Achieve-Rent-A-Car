f99737415d1bb460fe2aa86b0c4faac43cfead3e
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-04-24 10:50:57 +0100

    auto scroll up

--- resources/js/system.js
+++ resources/js/system.js
@@ -3,6 +3,15 @@ if (system) {
   system.title = 'Anyrent Bookings System';
   system.onload = function() {
     let systemLoader = document.getElementById('anyrent-skeleton');
+    let systemHeight = systemLoader.parentNode.offsetHeight+'px';
     systemLoader.style.display = 'none';
+    setInterval(function() {
+      if(systemHeight !== system.style.height) {
+        systemHeight = system.style.height;
+        document.getElementById("anyrent").scrollIntoView({
+          behavior: "instant"
+        });
+      }
+    }, 50);
   }
 }
