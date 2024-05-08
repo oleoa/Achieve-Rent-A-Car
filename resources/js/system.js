01740b7b86ef8ddbda8d873eb1d752230f012733
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-05-08 15:23:40 +0100

    no scroll for now

--- resources/js/system.js
+++ resources/js/system.js
@@ -1,13 +1,4 @@
 let system = document.getElementById('anyrentBookingsIframe');
 if (system) {
   system.title = 'Anyrent Bookings System';
-  let systemHeight = systemLoader.parentNode.offsetHeight+'px';
-  setInterval(function() {
-    if(systemHeight !== system.style.height) {
-      systemHeight = system.style.height;
-      document.getElementById("anyrent").scrollIntoView({
-        behavior: "instant"
-      });
-    }
-  }, 50);
 }
