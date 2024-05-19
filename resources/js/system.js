b06777177b00afb32ab391375d7185f97f163563
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-05-19 10:54:39 +0100

    debugging

--- resources/js/system.js
+++ resources/js/system.js
@@ -5,6 +5,7 @@ if (system) {
   window.addEventListener("message", function(event) {
     if (event.source === system.contentWindow) {
       if (event.data.type === "arIframeUrlChange") {
+        console.log('enters in the event.data.type === "arIframeUrlChange');
         var newURL = event.data.url;
         //console.debug("URL mudou: " + newURL);
         console.log(JSON.stringify(event.data));
