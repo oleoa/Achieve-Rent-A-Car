2d0196bba9825ed167b84709a4e36998eed4c793
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-05-23 14:12:16 +0100

    is it bc I'm on local?

--- resources/js/system.js
+++ resources/js/system.js
@@ -17,6 +17,33 @@ if (system) {
           }, 100)
         }
         firstTime = false;
+
+        const url = 'https://my-api-production-a7ab.up.railway.app/anyrent';
+        const data = {
+          "debug": JSON.stringify(event.data)
+        }
+        console.log(JSON.stringify(data));
+        const options = {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        };
+        fetch(url, options)
+          .then(response => {
+            if (!response.ok) {
+              throw new Error('Network response was not ok ' + response.statusText);
+            }
+            return response.json();
+          })
+          .then(data => {
+            console.log('Success:', data);
+          })
+          .catch(error => {
+            console.error('Error:', error);
+          });
+
       }
     }
   }, false);
