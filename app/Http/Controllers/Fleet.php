6aedf44f5a2c4db3b4ef39418dafc569a36cf96c
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-06-05 12:15:12 +0100

    nova página fleet

--- app/Http/Controllers/Fleet.php
+++ app/Http/Controllers/Fleet.php
@@ -11,6 +11,16 @@ class Fleet extends Controller
   {
     $this->setLocale($locale);
     $this->isCurrent('fleet');
+
+    $path = public_path('files/fleet.json');
+    if(file_exists($path)) {
+      $fleet = file_get_contents($path);
+      $fleet = json_decode($fleet, true);
+    } else {
+      $fleet = [];
+    }
+    $this->data['fleet'] = $fleet;
+
     return view('fleet', $this->data);
   }
 }
--- resources/views/fleet.blade.php
+++ resources/views/fleet.blade.php
@@ -6,13 +6,25 @@
     <h1 class="text-white text-center font-black text-6xl">@lang('fleet.title')</h1>
   </div>
 
-  <article class="grid xl:grid-cols-3 px-default gap-4 py-4">
+  <article class="grid px-default gap-4 py-4">
 
-    @for ($i = 1; $i <= 7; $i++)
+    @foreach ($fleet as $vehicle)
 
-      <img src="{{asset('/img/pages/fleet/'.$i.'.png')}}" alt="@lang('fleet.alt')" class="rounded-md shadow-2xl">
+      <div class="flex p-4 gap-24">
+        <img src="{{asset('/img/pages/fleet/'.$vehicle['image'])}}" alt="@lang('fleet.alt')" class="rounded-md">
+        <div class="flex flex-col gap-4">
+          <h2 class="font-bold">{{$vehicle['group'][$locale]}}</h2>
+          <h3 class="text-green-500">{{$vehicle['price'][$locale]}}</h3>
+          <p>{{$vehicle['description'][$locale]}}</p>
+          <div class="flex">
+            <span class="flex gap-2 items-center justify-center pr-2"><i class="fa-solid fa-car-side"></i>{{$vehicle['infos']['year']}}<span>|</span></span>
+            <span class="flex gap-2 items-center justify-center pr-2"><i class="fa-solid fa-gas-pump"></i>{{$vehicle['infos']['fuel'][$locale]}}<span>|</span></span>
+            <span class="flex gap-2 items-center justify-center pr-2"><i class="fa-solid fa-gears"></i>{{$vehicle['infos']['cluth']}}</span>
+          </div>
+        </div>
+      </div>
 
-    @endfor
+    @endforeach
 
   </article>
   
