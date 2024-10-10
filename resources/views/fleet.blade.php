9cc49cbb00f8adcb58604ea0a959ba699ed074c5
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-10-10 12:20:17 +0100

    new fleet updated

--- resources/views/fleet.blade.php
+++ resources/views/fleet.blade.php
@@ -14,16 +14,16 @@
         <img src="{{asset('/img/pages/fleet/'.$vehicle['image'])}}" alt="@lang('fleet.alt')" class="rounded-md">
         <h2 class="font-bold">{{$vehicle['group'][$locale]}}</h2>
         <div class="flex">
-          <span class="flex gap-2 items-center justify-center pr-2"><i class="fa-solid fa-door-open"></i>{{$vehicle['infos']['doors']}}<span>|</span></span>
-          <span class="flex gap-2 items-center justify-center pr-2"><i class="fa-solid fa-gears"></i>{{$vehicle['infos']['cluth']}}<span>|</span></span>
-          <span class="flex gap-2 items-center justify-center pr-2"><i class="fa-solid fa-car-on"></i>{{$vehicle['infos']['cc']}}<span>|</span></span>
+          <span class="flex gap-2 items-center justify-center pr-2"><img src="{{asset('/img/pages/fleet/door.svg')}}" alt="CC" class="h-4"></i>{{$vehicle['infos']['doors']}}<span>|</span></span>
+          <span class="flex gap-2 items-center justify-center pr-2"><img src="{{asset('/img/pages/fleet/cluth.svg')}}" alt="CC" class="h-4">{{$vehicle['infos']['cluth']}}<span>|</span></span>
+          <span class="flex gap-2 items-center justify-center pr-2"><img src="{{asset('/img/pages/fleet/cc.svg')}}" alt="CC" class="h-4">{{$vehicle['infos']['cc']}}<span>|</span></span>
           <span class="flex gap-2 items-center justify-center pr-2"><i class="fa-solid fa-snowflake"></i>{{$vehicle['infos']['cond']}}</span>
         </div>
         <div class="flex">
           <span class="flex gap-2 items-center justify-center pr-2"><i class="fa-solid fa-users"></i>{{$vehicle['infos']['people']}}<span>|</span></span>
           <span class="flex gap-2 items-center justify-center pr-2"><i class="fa-solid fa-gas-pump"></i>{{$vehicle['infos']['fuel'][$locale]}}<span>|</span></span>
           <span class="flex gap-2 items-center justify-center pr-2"><i class="fa-solid fa-horse-head"></i>{{$vehicle['infos']['horse']}}<span>|</span></span>
-          <span class="flex gap-2 items-center justify-center pr-2"><i class="fa-solid fa-gears"></i>{{$vehicle['infos']['steering']}}</span>
+          <span class="flex gap-2 items-center justify-center pr-2"><img src="{{asset('/img/pages/fleet/steering.svg')}}" alt="CC" class="h-4"></i>{{$vehicle['infos']['steering']}}</span>
         </div>
         <a target="_blank" href="{{"https://wa.me/+351938342970?text=".$text.$vehicle['group'][$locale]}}" class="px-4 py-2 rounded-lg bg-green-600 hover:bg-green-400 w-fit text-white">@lang('fleet.button')</a>
       </div>
