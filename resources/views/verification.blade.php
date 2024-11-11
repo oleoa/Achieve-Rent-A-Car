64a1074853defeae11fc1c97b712dae9d377f507
Leonardo <leonardo.abreu.de.paulo@gmail.com>
2024-11-11 17:19:27 +0000

    reloads the verification every 1 hour

--- resources/views/verification.blade.php
+++ resources/views/verification.blade.php
@@ -3,4 +3,12 @@
 
   <img src="{{asset('/img/verification/'.$status.'.png')}}" alt="Status" class="w-full">
 
+  <script>
+
+    setTimeout(() => {
+        location.reload();
+    }, 3600 * 1000); // 1 hour = 3600 seconds * 1000 milliseconds
+
+  </script>
+
 @endsection
